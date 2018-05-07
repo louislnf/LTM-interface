e45a70709cbafa6328d92dacc19fc7c910b29c3f
Louis LENIEF <lenief.louis@gmail.com>
2018-05-07 16:01:16 +0200

    Initial commit

---
+++ LTM-interface/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LTM-interface
+//
+//  Created by Louis Lenief on 07/05/2018.
+//  Copyright © 2018 Louis Lenief. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
